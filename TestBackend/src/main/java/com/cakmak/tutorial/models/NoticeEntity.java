d6da9664e6afc687f16bf6732318ba291507df2d
Mukail Cakmak <tr.mukailcakmak@gmail.com>
2022-08-29 16:32:27 +0300

    [MukailCakmak] entity teklestirildi

--- TestBackend/src/main/java/com/cakmak/tutorial/models/NoticeEntity.java
+++ TestBackend/src/main/java/com/cakmak/tutorial/models/NoticeEntity.java
@@ -3,75 +3,13 @@ package com.cakmak.tutorial.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "notice")
-public class NoticeEntity {
+@DiscriminatorValue("NOTICE")
+public class NoticeEntity extends ActivityEntity{
+    public NoticeEntity() {
+        super();
+    }
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-
-	@Column(name = "title")
-	private String title;
-
-	@Column(name = "description")
-	private String description;
-
-	@Column(name = "detail")
-	private String detail;
-
-	@Column(name = "published")
-	private boolean published;
-
-
-	public NoticeEntity() {
-
-	}
-
-	public NoticeEntity(String title, String description, boolean published) {
-		this.title = title;
-		this.description = description;
-		this.published = published;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public boolean isPublished() {
-		return published;
-	}
-
-	public void setPublished(boolean isPublished) {
-		this.published = isPublished;
-	}
-
-	public String getDetail() {
-		return detail;
-	}
-
-	public void setDetail(String detail) {
-		this.detail = detail;
-	}
-
-	@Override
-	public String toString() {
-		return "Notice [id=" + id + ", title=" + title + ", desc=" + description +  ", detail=" + detail + ", published=" + published + "]";
-	}
-
+    public NoticeEntity(String title, String description, boolean published) {
+        super(title,description,published);
+    }
 }
