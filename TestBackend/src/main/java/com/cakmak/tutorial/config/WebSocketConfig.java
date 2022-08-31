98d13667d848bf1b2c6cfd653ba517ca73216679
Mukail Cakmak <tr.mukailcakmak@gmail.com>
2022-08-31 18:59:05 +0300

    [MukailCakmak] websocket crate topic

--- TestBackend/src/main/java/com/cakmak/tutorial/config/WebSocketConfig.java
+++ TestBackend/src/main/java/com/cakmak/tutorial/config/WebSocketConfig.java
@@ -13,14 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
-        te.setPoolSize(1);
-        te.setThreadNamePrefix("wss-heartbeat-thread-");
-        te.initialize();
-        config.enableSimpleBroker("/topic/create")
-                .setTaskScheduler(te);
-
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
     }
 
     @Override
